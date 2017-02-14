25bdf7b06ce1030678940c842dd558248fc5d8f7
Ehsan Khosroshahi <eb452@drexel.edu>
2017-02-14 17:27:53 -0500

    no message

--- src/singapore/SampleSingaporePVT_Processed.java
+++ src/singapore/SampleSingaporePVT_Processed.java
@@ -1,47 +1,73 @@
 package singapore;
 
+import java.awt.font.FontRenderContext;
 // class for saving the processed PVT data
 import java.util.Vector;
 
 import javax.print.attribute.standard.RequestingUserName;
+
+import org.apache.commons.collections4.functors.ForClosure;
 
 import analysis.Utilities;
 import analysis.Values;
 
 public class SampleSingaporePVT_Processed {
 
-	private Vector<PVTsamples> samples;
+	private Vector<DrivingPVTsamples> samples;
 
-	private class PVTsamples{
+	public class DrivingPVTsamples{
 		String protocol;
 		String ID;
 		String trialdate;
 		int[] lapses = new int[8];
 		double[] alertAve = new double[8];
+		double[] lanePosSD = new double[5];
+		public DrivingPVTsamples() {
+			for (int i = 0; i < alertAve.length; i++) 
+				alertAve[i] = -1;
+			for (int i = 0; i < lapses.length; i++) 
+				lapses[i] = -1;
+			for (int i = 0; i < lanePosSD.length; i++) 
+				lanePosSD[i] = -1;
+			
+		}
 	}
+	
 
 	public SampleSingaporePVT_Processed() {
-		samples = new Vector<PVTsamples>();
+		samples = new Vector<DrivingPVTsamples>();
+		
+		
 	}
 
+	public DrivingPVTsamples get(int i) {
+		return samples.elementAt(i);
+		
+	}
 	public String toString(int i){
 		String s = ";";
 		s = samples.get(i).protocol+","+samples.get(i).ID+","+ samples.get(i).trialdate +",,";
-		for (int j = 0; j < 8; j++) {
-			if (samples.get(i).lapses[j] > 0)
+		for (int j = 0; j < samples.get(i).lapses.length; j++) {
+			if (samples.get(i).lapses[j] >= 0)
 				s+= samples.get(i).lapses[j];
 			s+= ",";
 		}
 		s += ",";
-		for (int j = 0; j < 8; j++) {
-			if (samples.get(i).alertAve[j] > 0)
+		for (int j = 0; j < samples.get(i).alertAve.length; j++) {
+			if (samples.get(i).alertAve[j] >= 0)
 				s+= Utilities.df1.format(samples.get(i).alertAve[j]);
+			s+= ",";
+		}
+		s += ",";
+		for (int j = 0; j < samples.get(i).lanePosSD.length; j++) {
+			if (samples.get(i).lanePosSD[j] >= 0)
+				s+= Utilities.df4.format(samples.get(i).lanePosSD[j]);
 			s+= ",";
 		}
 		return s;
 	}
 
-	public void add(SampleSingaporePVT sample){
+	public void addPVTsample(SampleSingaporePVT sample){
 		//processing the pvt result
 		int lapses = 0;
 		int falseStarts = 0;
@@ -57,38 +83,50 @@ public class SampleSingaporePVT_Processed {
 			}
 		}		
 		
-		if (samples.size() == 0){
-			PVTsamples s = new PVTsamples();
+		
+		boolean newData = true;
+		for (int i = 0; i < samples.size(); i++) {
+			if (samples.get(i).protocol.equals(sample.getProtocol()) && samples.get(i).ID.equals(sample.getId())
+					&& samples.get(i).trialdate.equals(sample.getTrialdate())){
+				samples.get(i).lapses[Integer.parseInt(sample.getTrial())] = lapses;
+				samples.get(i).alertAve[Integer.parseInt(sample.getTrial())] = alertResponses.average();
+				newData =false;
+			}
+		}
+		if (newData) {
+			DrivingPVTsamples s = new DrivingPVTsamples();
 			s.ID = sample.getId();
 			s.protocol = sample.getProtocol();
 			s.trialdate = sample.getTrialdate();
 			s.lapses[Integer.parseInt(sample.getTrial())] = lapses;
 			s.alertAve[Integer.parseInt(sample.getTrial())] = alertResponses.average();
 			samples.add(s);
-		}
-		else{
-			boolean newData = true;
-			for (int i = 0; i < samples.size(); i++) {
-				if (samples.get(i).protocol.equals(sample.getProtocol()) && samples.get(i).ID.equals(sample.getId())
-						&& samples.get(i).trialdate.equals(sample.getTrialdate())){
-					samples.get(i).lapses[Integer.parseInt(sample.getTrial())] = lapses;
-					samples.get(i).alertAve[Integer.parseInt(sample.getTrial())] = alertResponses.average();
-					newData =false;
-				}
-			}
-			if (newData) {
-				PVTsamples s = new PVTsamples();
-				s.ID = sample.getId();
-				s.protocol = sample.getProtocol();
-				s.trialdate = sample.getTrialdate();
-				s.lapses[Integer.parseInt(sample.getTrial())] = lapses;
-				s.alertAve[Integer.parseInt(sample.getTrial())] = alertResponses.average();
-				samples.add(s);
 
-			}	
-		}
+		}	
+		
 	}
 
+	public void addDrivingSample(String Protocol, String ID, String trialDate, String trial, double lanePosSD){
+
+
+		boolean newData = true;
+		for (int i = 0; i < samples.size(); i++) {
+			if (samples.get(i).protocol.equals(Protocol) && samples.get(i).ID.equals(ID)
+					&& samples.get(i).trialdate.equals(trialDate)){
+				samples.get(i).lanePosSD[Integer.parseInt(trial)] = lanePosSD;
+				newData =false;
+			}
+		}
+		if (newData) {
+			DrivingPVTsamples s = new DrivingPVTsamples();
+			s.ID = ID;
+			s.protocol = Protocol;
+			s.trialdate = trialDate;
+			s.lanePosSD[Integer.parseInt(trial)] = lanePosSD;
+			samples.add(s);
+		}	
+
+	}
 
 	public int size(){
 		return samples.size();
--- src/singapore/SingaporeDriving.java
+++ src/singapore/SingaporeDriving.java
@@ -30,6 +30,15 @@ public class SingaporeDriving {
 				+ "Driving data - standard deviation lateral position (Singapore)/"
 				+ "MFPD_PVT_all.txt");
 		PVTsamples = (new SingaporePVT(PVTfile)).samples;
+
+		File directoryA = new File ("/Users/ehsanebk/OneDrive - drexel.edu/"
+				+ "Driving data - standard deviation lateral position (Singapore)/"
+				+ "Driving Data/Protocol A filtered");
+		
+		File directoryB = new File ("/Users/ehsanebk/OneDrive - drexel.edu/"
+				+ "Driving data - standard deviation lateral position (Singapore)/"
+				+ "Driving Data/Protocol B filtered");
+		
 		
 		File PVTfileOutPutProcessed = new File("/Users/ehsanebk/OneDrive - drexel.edu/"
 				+ "Driving data - standard deviation lateral position (Singapore)/"
@@ -41,13 +50,124 @@ public class SingaporeDriving {
 			e1.printStackTrace();
 		}
 		foutPVT.println("protocol,id,trialdate,,lapses 0, lapses 1,lapses 2,lapses 3,lapses 4,lapses 5,lapses 6,lapses 7,"
-				+ ",alert ave 0,alert ave 1,alert ave 2,alert ave 3,alert ave 4,alert ave 5,alert ave 6,alert ave 7");
+				+ ",alert ave 0,alert ave 1,alert ave 2,alert ave 3,alert ave 4,alert ave 5,alert ave 6,alert ave 7,"
+				+ ",driving 0,driving 1,driving 2,driving 3,driving 4");
 		foutPVT.flush();
 		
 
 		for (int i = 0; i < PVTsamples.size(); i++) {
-			PVTprocessed.add(PVTsamples.elementAt(i));
-		}	
+			PVTprocessed.addPVTsample(PVTsamples.elementAt(i));
+		}
+		
+		// adding driving data
+		for (int i = 0; i < PVTsamples.size()-1; i++) {
+			Values LateralPositions = new Values();
+			String protocol = PVTsamples.elementAt(i).getProtocol();
+			String ID = PVTsamples.elementAt(i).getId();
+			String trial = PVTsamples.elementAt(i).getTrial();
+			String trialDate =PVTsamples.elementAt(i).getTrialdate();
+			String trialTimeString =PVTsamples.elementAt(i).getTrialtimeString();
+			int trialTime = PVTsamples.elementAt(i).getTrialtime();
+			int frameCount = 0;
+
+			
+			
+			if (ID.equals(PVTsamples.elementAt(i+1).getId())){  // check to see if it's the last trial of the day or not
+				System.out.println("time pvt : " + trialTime);
+				if (protocol.equals("A") && Integer.valueOf(trial).intValue()!=5){
+					for (File file : directoryA.listFiles())
+						if (file.getName().startsWith(ID)){
+							Vector<Double> LanePosAve= new Vector<Double>();
+
+							Tokenizer t = new Tokenizer(file);
+							t.skipLine(); // skiping the first line
+							while (t.hasMoreTokens()){
+								String Video = t.nextToken();
+								String Time = t.nextToken();
+								String LeftRho = t.nextToken();
+								String LeftTheta = t.nextToken();
+								String RightRho = t.nextToken();
+								String RightTheta = t.nextToken();
+								String LeftX = t.nextToken();
+								String RightX = t.nextToken();
+								String LaneWidth = t.nextToken();
+								String LaneCenter = t.nextToken();
+								double LateralPosition = t.nextDouble();
+								int LeftRhoThetaOK = t.nextInt();
+								int RightRhoThetaOK = t.nextInt();
+								int LeftSigSpikeOK = t.nextInt();
+								int RightSigSpikeOK = t.nextInt();
+								int LaneWidthOK = t.nextInt();
+								int isDriving = t.nextInt();
+
+								int timeDrivingFrame = Integer.valueOf(Time.substring(0, 5).replace(":", "")).intValue(); // getting the time in hhmm format
+
+
+								if (timeDrivingFrame > addTime(trialTime,5) && timeDrivingFrame <= addTime(PVTsamples.elementAt(i+1).getTrialtime(),5)){
+									LateralPositions.add(LateralPosition);
+									frameCount++;
+								}
+								else if (timeDrivingFrame > PVTsamples.elementAt(i+1).getTrialtime())
+									break; //breaking the while loop
+
+							}
+							if (frameCount > 0){
+
+								PVTprocessed.addDrivingSample(protocol, ID, trialDate, trial, LateralPositions.stddev());
+								 // LateralPositions.average()
+	
+							}
+							break;
+						}
+				}	
+				else if (protocol.equals("B") && Integer.valueOf(trial).intValue() != 2){
+					for (File file : directoryB.listFiles())
+						if (file.getName().startsWith(ID)){
+							Vector<Double> LanePosAve= new Vector<Double>();
+
+							Tokenizer t = new Tokenizer(file);
+							t.skipLine(); // skiping the first line
+							while (t.hasMoreTokens()){
+								String Video = t.nextToken();
+								String Time = t.nextToken();
+								String LeftRho = t.nextToken();
+								String LeftTheta = t.nextToken();
+								String RightRho = t.nextToken();
+								String RightTheta = t.nextToken();
+								String LeftX = t.nextToken();
+								String RightX = t.nextToken();
+								String LaneWidth = t.nextToken();
+								String LaneCenter = t.nextToken();
+								double LateralPosition = t.nextDouble();
+								int LeftRhoThetaOK = t.nextInt();
+								int RightRhoThetaOK = t.nextInt();
+								int LeftSigSpikeOK = t.nextInt();
+								int RightSigSpikeOK = t.nextInt();
+								int LaneWidthOK = t.nextInt();
+								int isDriving = t.nextInt();
+
+								int timeDrivingFrame = Integer.valueOf(Time.substring(0, 5).replace(":", "")).intValue(); // getting the time in hhmm format
+
+
+								if (timeDrivingFrame > addTime(trialTime,5) && timeDrivingFrame <= addTime(PVTsamples.elementAt(i+1).getTrialtime(),5)){
+									LateralPositions.add(LateralPosition);
+									frameCount++;
+								}
+								else if (timeDrivingFrame > PVTsamples.elementAt(i+1).getTrialtime())
+									break; //breaking the while loop
+
+							}
+
+							if (frameCount > 0){
+								PVTprocessed.addDrivingSample(protocol, ID, trialDate, trial, LateralPositions.stddev());
+								 // LateralPositions.average()
+							}
+							break;
+						}
+				}			
+			}
+		}
+		
 		for (int i = 0; i < PVTprocessed.size(); i++) {
 			foutPVT.println(PVTprocessed.toString(i));
 			foutPVT.flush();
@@ -55,6 +175,7 @@ public class SingaporeDriving {
 		foutPVT.flush();
 		foutPVT.close();
 	}
+	
 	
 	public static void 	Driving_vs_PVT_pure(){
 		// getting all the pvt data 
